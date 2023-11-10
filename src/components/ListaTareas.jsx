83d694335347843c9a33d58563cd763e7601fabf
Andres Jimenez <andresjimenez884@gmail.com>
2023-11-09 23:41:46 -0400

    Actualizaciones varias

--- src/components/ListaTareas.jsx
+++ src/components/ListaTareas.jsx
@@ -20,24 +20,32 @@ const ListaTareas = () => {
       </header>
 
       <div className="grid grid-cols-3 gap-4">
+
         {taskState.map((task) => (
+
           <div
             key={task.id}
             className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col"
           >
+
             <div className="mb-6">
+
               <div className="font-bold text-xl mb-2">{task.title}</div>
               <p className="text-gray-700 text-base">{task.description}</p>
+
               <div className="flex items-center justify-between">
+
                 <Link to={`/update-task/${task.id}`} className="bg-blue-500">
                   Actualizar
                 </Link>
+
                 <button
                   className="bg-red-500"
                   onClick={() => handleDelete(task.id)}
                 >
                   Eliminar
                 </button>
+                
               </div>
             </div>
           </div>
